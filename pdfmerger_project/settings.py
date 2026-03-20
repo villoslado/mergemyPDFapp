b718927f06aa72dfd4c7caefbb7a8b5eacdf1bfd
Gerardo Villoslado <jg.villoslado@gmail.com>
2026-03-20 14:08:35 -0700

    Simplify WhiteNoise static storage for Railway

--- pdfmerger_project/settings.py
+++ pdfmerger_project/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = "staticfiles"
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
