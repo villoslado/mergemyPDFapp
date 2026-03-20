7ff0248a21b046d68b2ab6f954af2f110db76114
Gerardo Villoslado <jg.villoslado@gmail.com>
2026-03-20 13:17:38 -0700

    Configure Django settings for deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn pdfmerger_project.wsgi
+
--- pdfmerger_project/settings.py
+++ pdfmerger_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^$sxn0y&##*l#+_ny&e-!4bf@nc%ocj5o2oxi$jt%6whof#4t3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
