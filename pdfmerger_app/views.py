1e0bbb44018d89e3fdd71543c0632e47618c99e2
Gerardo Villoslado <jg.villoslado@gmail.com>
2026-03-23 22:12:58 -0700

    Fix success state: replace cookie polling with iframe onload approach

--- pdfmerger_app/views.py
+++ pdfmerger_app/views.py
@@ -37,7 +37,6 @@ def merge_view(request):
                             'attachment; filename="merged.pdf"'
                         )
 
-                    response.set_cookie('merge_complete', '1', max_age=60, samesite='Lax')
                     return response
 
                 finally:
